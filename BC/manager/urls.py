893cfe7dfb1466e07d748818f4636f93ebf3cc76
Mupang <eorhkd7078@naver.com>
2025-12-05 10:30:58 +0900

    강대광  - url 수정중

--- BC/manager/urls.py
+++ BC/manager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'manager'
+
 urlpatterns = [
     path('', views.manager, name='manager_login'),
     path('dashboard/', views.dashboard, name='dashboard'),
